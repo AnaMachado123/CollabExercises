94dab24449d9dbb0666c970b8c46cb7c246f51f1
Eduarda Gomes <eduarda.gomes@ipvc.pt>
2026-01-15 10:59:36 +0000

    register page working

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -38,17 +38,13 @@ function Register() {
     }
 
     try {
-      const res = await fetch("http://localhost:3000/api/auth/register", {
+      const res = await fetch("/api/auth/register", {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name,
-          email,
-          password,
-        }),
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name, email, password }),
       });
+
+
 
       const data = await res.json();
 
